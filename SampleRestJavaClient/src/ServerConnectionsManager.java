c0a2c55f65474ce481f8fc958c7def283d697a2a
Andre Leibovici <andre_leibovici@yahoo.com>
2014-08-16 23:09:08 -0700

    Moved iterators in Test.java to ServerConnectionManager.java Addded getAlerts, addVMstoProtectionDomain, removeVmsFromProtectionDomain
    Signed-off-by: Andre Leibovici <andre_leibovici@yahoo.com>

--- SampleRestJavaClient/src/ServerConnectionsManager.java
+++ SampleRestJavaClient/src/ServerConnectionsManager.java
@@ -7,7 +7,11 @@
  */
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.net.ssl.TrustManager;
 
@@ -20,6 +24,23 @@ import org.apache.cxf.transport.http.HTTPConduit;
 import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
 import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 
+import com.nutanix.prism.dto.alerts.AlertDTO;
+import com.nutanix.prism.dto.appliance.configuration.ContainerDTO;
+import com.nutanix.prism.dto.appliance.configuration.DiskDTO;
+import com.nutanix.prism.dto.appliance.configuration.ManagementServerDTO;
+import com.nutanix.prism.dto.appliance.configuration.NodeDTO;
+import com.nutanix.prism.dto.dr.AddVMsToPdRequestDTO;
+import com.nutanix.prism.dto.dr.ProtectionDomainDTO;
+import com.nutanix.prism.dto.health.check.HealthStatus;
+import com.nutanix.prism.dto.stats.VMDTO;
+import com.nutanix.prism.exception.alerts.AlertsAdministrationException;
+import com.nutanix.prism.exception.appliance.configuration.ContainerAdministrationException;
+import com.nutanix.prism.exception.appliance.configuration.DiskAdministrationException;
+import com.nutanix.prism.exception.appliance.configuration.HostAdministrationException;
+import com.nutanix.prism.exception.appliance.configuration.ManagementServerAdministrationException;
+import com.nutanix.prism.exception.dr.BackupAndDrAdministrationException;
+import com.nutanix.prism.exception.vmmanagement.VMAdministrationException;
+import com.nutanix.prism.services.alerts.AlertsAdministration;
 import com.nutanix.prism.services.appliance.configuration.ContainerAdministration;
 import com.nutanix.prism.services.appliance.configuration.DiskAdministration;
 import com.nutanix.prism.services.appliance.configuration.HostAdministration;
@@ -134,7 +155,7 @@ public final class ServerConnectionsManager implements Cloneable {
 	 * 
 	 * @return DiskAdministration
 	 */
-	public DiskAdministration getDiskAdminRef() {
+	private DiskAdministration getDiskAdminRef() {
 		return getRestService(serverhost, username, password,
 				DiskAdministration.class);
 	}
@@ -144,7 +165,7 @@ public final class ServerConnectionsManager implements Cloneable {
 	 * 
 	 * @return HostAdministration
 	 */
-	public HostAdministration getHostAdminRef() {
+	private HostAdministration getHostAdminRef() {
 		return getRestService(serverhost, username, password,
 				HostAdministration.class);
 	}
@@ -154,7 +175,7 @@ public final class ServerConnectionsManager implements Cloneable {
 	 * 
 	 * @return ContainerAdministration
 	 */
-	public ContainerAdministration getContainerAdminRef() {
+	private ContainerAdministration getContainerAdminRef() {
 		return getRestService(serverhost, username, password,
 				ContainerAdministration.class);
 	}
@@ -164,7 +185,7 @@ public final class ServerConnectionsManager implements Cloneable {
 	 * 
 	 * @return ManagementServerAdministration
 	 */
-	public ManagementServerAdministration getManagementServerAdminRef() {
+	private ManagementServerAdministration getManagementServerAdminRef() {
 		return getRestService(serverhost, username, password,
 				ManagementServerAdministration.class);
 	}
@@ -173,7 +194,7 @@ public final class ServerConnectionsManager implements Cloneable {
 	 * 
 	 * @return VMAdministration
 	 */
-	public VMAdministration getVMAdministration() {
+	private VMAdministration getVMAdministration() {
 		return getRestService(serverhost, username, password,
 				VMAdministration.class);
 	}
@@ -182,9 +203,218 @@ public final class ServerConnectionsManager implements Cloneable {
 	 * 
 	 * @return BackupAndDrAdministration
 	 */
-	public BackupAndDrAdministration getBackupAndDrAdministration() {
+	private BackupAndDrAdministration getBackupAndDrAdministration() {
 		return getRestService(serverhost, username, password,
 				BackupAndDrAdministration.class);
+	}
+
+	/**
+	 * 
+	 * @return AlertService
+	 */
+	private AlertsAdministration getAlertService() {
+		return getRestService(serverhost, username, password,
+				AlertsAdministration.class);
+	}
+
+	/**
+	 * Method getAlerts
+	 * 
+	 * @param count
+	 * @param resolved
+	 * @param acknowledged
+	 * @param connMgr
+	 * @return Iterator<AlertDTO>
+	 * @throws AlertsAdministrationException
+	 */
+	public Iterator<AlertDTO> getAlerts(int count, boolean resolved,
+			boolean acknowledged, ServerConnectionsManager connMgr)
+			throws AlertsAdministrationException {
+		final AlertsAdministration ref = connMgr.getAlertService();
+		final List<AlertDTO> alerts = ref.getAlerts(null, null, count,
+				resolved, acknowledged).getEntities();
+		final Iterator<AlertDTO> iteratorAlerts = alerts.iterator();
+		return iteratorAlerts;
+	}
+
+	/**
+	 * Method getDisks
+	 * 
+	 * @param connMgr
+	 * @return Iterator<DiskDTO>
+	 * @throws DiskAdministrationException
+	 */
+	public Iterator<DiskDTO> getDisks(ServerConnectionsManager connMgr)
+			throws DiskAdministrationException {
+		final DiskAdministration ref = connMgr.getDiskAdminRef();
+		final Collection<DiskDTO> disks = ref.getDisks(null, null, null)
+				.getEntities();
+		final Iterator<DiskDTO> iteratorDisks = disks.iterator();
+		return iteratorDisks;
+	}
+
+	/**
+	 * Method getHosts
+	 * 
+	 * @param connMgr
+	 * @return Iterator<NodeDTO>
+	 * @throws HostAdministrationException
+	 */
+	public Iterator<NodeDTO> getHosts(ServerConnectionsManager connMgr)
+			throws HostAdministrationException {
+		final HostAdministration ref1 = connMgr.getHostAdminRef();
+		final List<NodeDTO> hosts = ref1.getHosts(null, null, null)
+				.getEntities();
+		final Iterator<NodeDTO> iteratorHosts = hosts.iterator();
+		return iteratorHosts;
+	}
+
+	/**
+	 * Method getHostsHealthCheckSummary
+	 * 
+	 * @param connMgr
+	 * @return Map<HealthStatus, Integer>
+	 * @throws HostAdministrationException
+	 */
+	@SuppressWarnings("unused")
+	private Map<HealthStatus, Integer> getHostsHealthSummary(
+			final ServerConnectionsManager connMgr)
+			throws HostAdministrationException {
+		final HostAdministration ref1 = connMgr.getHostAdminRef();
+		final Map<HealthStatus, Integer> healthSummary = ref1
+				.getHealthCheckSummaryForHosts(null, false).getHealthSummary();
+		return healthSummary;
+	}
+
+	/**
+	 * Method getProtectionDomainsHealthSummary
+	 * 
+	 * @param connMgr
+	 * @return healthSummary
+	 * @throws BackupAndDrAdministrationException
+	 */
+	@SuppressWarnings("unused")
+	private Map<HealthStatus, Integer> getProtectionDomainsHealthSummary(
+			final ServerConnectionsManager connMgr)
+			throws BackupAndDrAdministrationException {
+		final BackupAndDrAdministration ref = connMgr
+				.getBackupAndDrAdministration();
+		final Map<HealthStatus, Integer> healthSummary = ref
+				.getHealthCheckSummaryForProtectionDomains(null, false)
+				.getHealthSummary();
+		return healthSummary;
+	}
+
+	/**
+	 * Method getContainers
+	 * 
+	 * @param connMgr
+	 * @return Iterator<ContainerDTO>
+	 * @throws ContainerAdministrationException
+	 */
+	public Iterator<ContainerDTO> getContainers(ServerConnectionsManager connMgr)
+			throws ContainerAdministrationException {
+		final ContainerAdministration ref2 = connMgr.getContainerAdminRef();
+		final List<ContainerDTO> containers = ref2.getContainers(null, null,
+				null).getEntities();
+		final Iterator<ContainerDTO> iteratorContainers = containers.iterator();
+		return iteratorContainers;
+	}
+
+	/**
+	 * Method getManagementServers
+	 * 
+	 * @param connMgr
+	 * @return Iterator<ManagementServerDTO>
+	 * @throws ManagementServerAdministrationException
+	 */
+	public Iterator<ManagementServerDTO> getManagementServers(
+			ServerConnectionsManager connMgr)
+			throws ManagementServerAdministrationException {
+		final ManagementServerAdministration ref3 = connMgr
+				.getManagementServerAdminRef();
+		final List<ManagementServerDTO> managementServers = ref3
+				.getManagementServers(null);
+		final Iterator<ManagementServerDTO> iteratorManagementServers = managementServers
+				.iterator();
+		return iteratorManagementServers;
+	}
+
+	/**
+	 * Method getVMs
+	 * 
+	 * @param connMgr
+	 * @return Iterator<VMDTO>
+	 * @throws VMAdministrationException
+	 */
+	public Iterator<VMDTO> getVMs(ServerConnectionsManager connMgr)
+			throws VMAdministrationException {
+		final VMAdministration ref4 = connMgr.getVMAdministration();
+		final List<VMDTO> vms = ref4.getVMs(null, null, null).getEntities();
+		final Iterator<VMDTO> iteratorVms = vms.iterator();
+		return iteratorVms;
+	}
+
+	/**
+	 * Method getProtectionDomains
+	 * 
+	 * @param connMgr
+	 * @return Iterator<ProtectionDomainDTO>
+	 * @throws BackupAndDrAdministrationException
+	 */
+	public Iterator<ProtectionDomainDTO> getProtectionDomains(
+			ServerConnectionsManager connMgr)
+			throws BackupAndDrAdministrationException {
+		final BackupAndDrAdministration bcdr = connMgr
+				.getBackupAndDrAdministration();
+		final List<ProtectionDomainDTO> backupAndDrAdministration = bcdr
+				.getProtectionDomains(null, true);
+		final Iterator<ProtectionDomainDTO> iteratorBackupAndDrAdministration = backupAndDrAdministration
+				.iterator();
+		return iteratorBackupAndDrAdministration;
+	}
+
+	/**
+	 * Method addVMstoProtectionDomain
+	 * 
+	 * @param vMs
+	 * @param protectionDomain
+	 * @param setAppConsistentSnapshots
+	 * @param setConsistencyGroupName
+	 * @param setIgnoreDupOrMissingVms
+	 * @param connMgr
+	 * @throws BackupAndDrAdministrationException
+	 */
+	public void addVMstoProtectionDomain(String vMs[], String protectionDomain,
+			Boolean setAppConsistentSnapshots, String setConsistencyGroupName,
+			Boolean setIgnoreDupOrMissingVms, ServerConnectionsManager connMgr)
+			throws BackupAndDrAdministrationException {
+		final List<String> list = Arrays.asList(vMs);
+		final AddVMsToPdRequestDTO vMsToPd = new AddVMsToPdRequestDTO();
+		vMsToPd.setNames(list);
+		vMsToPd.setAppConsistentSnapshots(setAppConsistentSnapshots);
+		vMsToPd.setConsistencyGroupName(setConsistencyGroupName);
+		vMsToPd.setIgnoreDupOrMissingVms(setIgnoreDupOrMissingVms);
+		final com.nutanix.prism.services.dr.BackupAndDrAdministration bcdr = connMgr
+				.getBackupAndDrAdministration();
+		bcdr.addVmsByNamesToProtectionDomain(protectionDomain, vMsToPd);
+	}
+
+	/**
+	 * Method removeVmsFromProtectionDomain
+	 * 
+	 * @param vMs
+	 * @param protectionDomain
+	 * @param connMgr
+	 * @throws BackupAndDrAdministrationException
+	 */
+	public void removeVmsFromProtectionDomain(String vMs[],
+			String protectionDomain, ServerConnectionsManager connMgr)
+			throws BackupAndDrAdministrationException {
+		final List<String> list = Arrays.asList(vMs);
+		final com.nutanix.prism.services.dr.BackupAndDrAdministration bcdr = connMgr
+				.getBackupAndDrAdministration();
+		bcdr.removeVmsFromProtectionDomain(protectionDomain, list);
 	}
 
 	/**
@@ -216,7 +446,6 @@ public final class ServerConnectionsManager implements Cloneable {
 	 */
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return super.toString();
 	}
 
--- SampleRestJavaClient/src/Test.java
+++ SampleRestJavaClient/src/Test.java
@@ -1,3 +1,4 @@
+// $codepro.audit.disable debuggingCode
 /**
  * Nutanix Java SDK Test Example
  * 
@@ -5,28 +6,22 @@
  * @author Andre Leibovici
  * @version 1.0
  */
-import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 
+import com.nutanix.prism.dto.alerts.AlertDTO;
 import com.nutanix.prism.dto.appliance.configuration.ContainerDTO;
 import com.nutanix.prism.dto.appliance.configuration.DiskDTO;
 import com.nutanix.prism.dto.appliance.configuration.ManagementServerDTO;
 import com.nutanix.prism.dto.appliance.configuration.NodeDTO;
 import com.nutanix.prism.dto.dr.ProtectionDomainDTO;
 import com.nutanix.prism.dto.stats.VMDTO;
+import com.nutanix.prism.exception.alerts.AlertsAdministrationException;
 import com.nutanix.prism.exception.appliance.configuration.ContainerAdministrationException;
 import com.nutanix.prism.exception.appliance.configuration.DiskAdministrationException;
 import com.nutanix.prism.exception.appliance.configuration.HostAdministrationException;
 import com.nutanix.prism.exception.appliance.configuration.ManagementServerAdministrationException;
 import com.nutanix.prism.exception.dr.BackupAndDrAdministrationException;
 import com.nutanix.prism.exception.vmmanagement.VMAdministrationException;
-import com.nutanix.prism.services.appliance.configuration.ContainerAdministration;
-import com.nutanix.prism.services.appliance.configuration.DiskAdministration;
-import com.nutanix.prism.services.appliance.configuration.HostAdministration;
-import com.nutanix.prism.services.appliance.configuration.ManagementServerAdministration;
-import com.nutanix.prism.services.dr.BackupAndDrAdministration;
-import com.nutanix.prism.services.vmmanagement.VMAdministration;
 
 /**
  * @author andreleibovici
@@ -42,89 +37,114 @@ public class Test implements Cloneable {
 	 * @throws ContainerAdministrationException
 	 * @throws BackupAndDrAdministrationException
 	 * @throws ManagementServerAdministrationException
+	 * @throws AlertsAdministrationException 
 	 * @throws Exception
 	 **/
 	public static void main(String[] args) throws VMAdministrationException,
 			DiskAdministrationException, HostAdministrationException,
 			ContainerAdministrationException,
 			BackupAndDrAdministrationException,
-			ManagementServerAdministrationException {
+			ManagementServerAdministrationException, AlertsAdministrationException {
 
-		// Stablish connection with PRISM API
+		/*
+		 * Stablish connection with PRISM API
+		 */
 		final ServerConnectionsManager connMgr = ServerConnectionsManager
 				.getInstance();
 		connMgr.setUSERNAME("admin");
 		connMgr.setPASSWORD("admin");
 		connMgr.setSERVER("10.20.18.10");
+		
+		/*
+		 * Instantiate PRISM objects
+		 */
+		final Iterator<VMDTO> iteratorVms = connMgr.getVMs(connMgr);	// VMs
+		final Iterator<DiskDTO> iteratorDisks = connMgr.getDisks(connMgr);	// Disks
+		final Iterator<NodeDTO> iteratorHosts = connMgr.getHosts(connMgr);	// Hosts
+		final Iterator<ContainerDTO> iteratorContainers = connMgr	// Containers
+				.getContainers(connMgr);
+		final Iterator<ManagementServerDTO> iteratorManagementServers = connMgr	// Management Servers
+				.getManagementServers(connMgr);
+		final Iterator<ProtectionDomainDTO> iteratorBackupAndDrAdministration = connMgr	// Backup and DR
+				.getProtectionDomains(connMgr);
+		final Iterator<AlertDTO> iteratorAlerts = connMgr.getAlerts(10, false, false, connMgr); // Alerts
 
-		// Retrieve list of virtual machines and properties
+		/*
+		 * Retrieve list of virtual machines and properties
+		 */
 		System.out.println();
 		System.out.println("Virtual Machines");
-		final VMAdministration ref4 = connMgr.getVMAdministration();
-		final List<VMDTO> vms = ref4.getVMs(null, null, null).getEntities();
-		final Iterator<VMDTO> iteratorVms = vms.iterator();
 		while (iteratorVms.hasNext()) {
 			System.out.println(iteratorVms.next());
 		}
 
-		// Retrieve list of disks and properties
+		/*
+		 * Retrieve list of disks and properties
+		 */
 		System.out.println();
 		System.out.println("Disks");
-		final DiskAdministration ref = connMgr.getDiskAdminRef();
-		final Collection<DiskDTO> disks = ref.getDisks(null, null, null)
-				.getEntities();
-		final Iterator<DiskDTO> iteratorDisks = disks.iterator();
 		while (iteratorDisks.hasNext()) {
 			System.out.println(iteratorDisks.next());
 		}
 
-		// Retrieve list of hosts and properties
+		/*
+		 * Retrieve list of hosts and properties
+		 */
 		System.out.println();
 		System.out.println("Hosts");
-		final HostAdministration ref1 = connMgr.getHostAdminRef();
-		final List<NodeDTO> hosts = ref1.getHosts(null, null, null)
-				.getEntities();
-		final Iterator<NodeDTO> iteratorHosts = hosts.iterator();
 		while (iteratorHosts.hasNext()) {
 			System.out.println(iteratorHosts.next());
 		}
 
-		// Retrieve list of containers and properties
+		/*
+		 * Retrieve list of containers and properties
+		 */
 		System.out.println();
 		System.out.println("Containers");
-		final ContainerAdministration ref2 = connMgr.getContainerAdminRef();
-		final List<ContainerDTO> containers = ref2.getContainers(null, null,
-				null).getEntities();
-		final Iterator<ContainerDTO> iteratorContainers = containers.iterator();
 		while (iteratorContainers.hasNext()) {
 			System.out.println(iteratorContainers.next());
 		}
 
-		// Retrieve list of management servers and properties
+		/*
+		 * Retrieve list of management servers and properties
+		 */
 		System.out.println();
 		System.out.println("Management Servers");
-		final ManagementServerAdministration ref3 = connMgr
-				.getManagementServerAdminRef();
-		final List<ManagementServerDTO> managementServers = ref3
-				.getManagementServers(null);
-		final Iterator<ManagementServerDTO> iteratorManagementServers = managementServers
-				.iterator();
 		while (iteratorManagementServers.hasNext()) {
 			System.out.println(iteratorManagementServers.next());
 		}
 
-		// Retrieve list of protection domains and protected vms
+		/*
+		 * Retrieve list of protection domains and protected vms
+		 */
 		System.out.println();
 		System.out.println("Protection Domains");
-		final BackupAndDrAdministration ref5 = connMgr
-				.getBackupAndDrAdministration();
-		final List<ProtectionDomainDTO> backupAndDrAdministration = ref5
-				.getProtectionDomains(null, true);
-		final Iterator<ProtectionDomainDTO> iteratorBackupAndDrAdministration = backupAndDrAdministration
-				.iterator();
 		while (iteratorBackupAndDrAdministration.hasNext()) {
 			System.out.println(iteratorBackupAndDrAdministration.next());
 		}
+
+		/*
+		 * Retrieve list of protection domains and protected vms
+		 */
+		System.out.println();
+		System.out.println("Alerts");
+		while (iteratorAlerts.hasNext()) {
+			System.out.println(iteratorAlerts.next());
+		}
+		
+		/*
+		 * Add VMs to Protection Domain
+		 */
+//		 String VMs[] = new String[] { "vmName" };
+//		 connMgr.addVMstoProtectionDomain(VMs, "protectionDomainName", false, null, false,
+//		 connMgr);
+		
+		/*
+		 * Remove VMs from Protection Domain
+		 */
+//		 String VMs[] = new String[] { "vmName" };
+//		 connMgr.removeVmsFromProtectionDomain(VMs, "protectionDomainName", connMgr);
+
 	}
 
 	/**
